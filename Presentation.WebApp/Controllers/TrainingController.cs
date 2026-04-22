797b583e93e3ccd574a8e6603c2db27ecc8281e7
PXLkaarlo <victor.l.immo@gmail.com>
2026-04-22 19:42:56 +0200

    Pretty Shure i'm done with the regular Controllers. Now onto AREAS

---
+++ Presentation.WebApp/Controllers/TrainingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.WebApp.Controllers;
+
+[Route("training")]
+public class TrainingController : Controller
+{
+    [HttpGet("group-training")]
+    [AllowAnonymous]
+    public IActionResult GroupTraining()
+    {
+        ViewData["Title"] = "Group Training";
+
+        return View();
+    }
+
+    [HttpGet("personal-training")]
+    [AllowAnonymous]
+    public IActionResult PersonalTrainer()
+    {
+        ViewData["Title"] = "Personal Training";
+
+        return View();
+    }
+
+    [HttpGet("online-coaching")]
+    [AllowAnonymous]
+    public IActionResult OnlineCoaching()
+    {
+        ViewData["Title"] = "Online Coaching";
+
+        return View();
+    }
+}
+// Have yet to find a place where this controller is used but it is here if needed.
